53dc3ccf6c8ffaf3a930d9ca0e50dbc27f2a48b3
Christian Raue <captain_2@gmx.de>
2023-10-16 15:18:52 +0200

    added Program class

---
+++ backend/program.py
@@ -0,0 +1,11 @@
+from backend import time_util as tu
+from backend.address import Address
+from backend.config import config
+from backend.hardware import hardware
+from backend.command import Command
+from backend.led import led
+
+
+class Program:
+
+    ...  # TODO

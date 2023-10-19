38d6e1d91b6219c3f6db288965639a22496eabbe
Christian Raue <captain_2@gmx.de>
2023-10-19 08:55:42 +0200

    added Request and Response classes

---
+++ backend/request.py
@@ -0,0 +1,95 @@
+import socket
+import json
+
+
+class Request:
+
+    _content: str
+    _socket: socket.socket
+    _client_address: str
+    _client_port: int
+
+    _method: str
+    _url: str
+    _headers: dict[str, str]
+    _payload: str
+    _location: str
+    _get_parameters: dict[str, str]
+
+    path_parameter: str | None
+
+    def __init__(
+        self, content: str, socket: socket.socket,
+        client_address: str, client_port: int
+    ):
+        self._content = content
+        self._socket = socket
+        self._client_address = client_address
+        self._client_port = client_port
+        self.path_parameter = None
+        self._parse_content()
+
+    def _parse_content(self):
+        lines = self._content.split("\n")
+        self._method, self._url, *_ = lines[0].split(" ")
+        self._headers = {}
+        for line in lines[1:]:
+            if line == "":
+                break
+            colon_index = line.index(":")
+            self._headers[line[0:colon_index].lower()] = (
+                line[colon_index + 2:]
+            )
+        self._payload = "\n".join(lines[len(self._headers) + 2])
+        if "?" in self._url:
+            self._location, parameter_string = self._url.split("?")
+            self._get_parameters = {
+                pair.split("=")[0]: pair.split("=")[1]
+                for pair in parameter_string.split("&")
+            }
+        else:
+            self._location = self._url
+            self._get_parameters = {}
+
+    @property
+    def socket(self) -> socket.socket:
+        return self._socket
+
+    @property
+    def method(self) -> str:
+        return self._method.upper()
+
+    @property
+    def client_address(self) -> str:
+        return self._client_address
+
+    @property
+    def client_port(self) -> str:
+        return self._client_port
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @property
+    def headers(self) -> dict[str, str]:
+        return self._headers
+
+    @property
+    def payload(self) -> str:
+        return self._payload
+
+    @property
+    def location(self) -> str:
+        return self._location
+
+    @property
+    def get_parameters(self) -> dict[str, str]:
+        return self._get_parameters
+
+    @property
+    def json_payload(self) -> dict:
+        try:
+            return json.loads(self._payload)
+        except json.decode.JSONDecodeError:
+            return {}
---
+++ backend/response.py
@@ -0,0 +1,66 @@
+class Response:
+
+    STATUS_CODES: dict[int, str] = {
+        200: "OK",
+        301: "Moved Permanently",
+        400: "Bad Request",
+        404: "Not Found",
+        405: "Method Not Allowed",
+        500: "Internal Server Error",
+        501: "Not Implemented"
+    }
+
+    CONTENT_TYPE_HTML: str = "text/html"
+    CONTENT_TYPE_JSON: str = "application/json"
+    CONTENT_TYPE_PLAIN: str = "text/plain"
+    CONTENT_TYPE_EVENT_STREAM: str = "text/event-stream"
+
+    _status_code: int
+    _headers: dict[str, str]
+    _keep_alive: bool
+
+    def __init__(
+        self,
+        status_code: int = 200,
+        body: str = "{}",
+        content_type: str = CONTENT_TYPE_JSON,
+        keep_alive: bool = False
+    ):
+        self._status_code = status_code
+        self._body = body
+        self._headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": content_type,
+            "Content-Length": str(len(body))
+        }
+        self._keep_alive = keep_alive
+
+    def add_header(self, key: str, value: str):
+        self._headers[key] = value
+
+    @property
+    def keep_alive(self) -> bool:
+        return self._keep_alive
+
+    @property
+    def status_code(self) -> int:
+        return self._status_code
+
+    @property
+    def status_line(self) -> str:
+        return (
+            f"HTTP/1.1 {self._status_code} "
+            f"{self.STATUS_CODES[self._status_code]}"
+        )
+
+    @property
+    def content(self) -> str:
+        return (
+            self.status_line + "\n"
+            + "\n".join(
+                f"{key}: {value}"
+                for key, value in self._headers.items()
+            )
+            + "\n\n"
+            + self._body
+        )

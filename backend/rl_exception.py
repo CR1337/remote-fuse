3033d309f9f81c4ea086b75ca380d3ad6c641628
Christian Raue <captain_2@gmx.de>
2023-11-11 10:20:30 +0100

    default empty message

--- backend/rl_exception.py
+++ backend/rl_exception.py
@@ -1,5 +1,5 @@
 class RlException(Exception):
 
-    def __init__(self, message: str):
-        self.message = message
+    def __init__(self, message: str = None):
+        self.message = message if message else ""
         super().__init__(self.message)
